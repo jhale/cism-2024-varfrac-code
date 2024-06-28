7d83e16d0ff971e9df3b355928a7c8eb508cf259
Jack S. Hale <mail@jackhale.co.uk>
2024-06-28 16:02:17 +0200

    Shorten.

--- code/stability_analysis/stability_analysis.py
+++ code/stability_analysis/stability_analysis.py
@@ -4,12 +4,6 @@
 #     text_representation:
 #       extension: .py
 #       format_name: light
-#       format_version: '1.5'
-#       jupytext_version: 1.16.2
-#   kernelspec:
-#     display_name: Python 3 (ipykernel)
-#     language: python
-#     name: python3
 # ---
 
 # + [markdown]
