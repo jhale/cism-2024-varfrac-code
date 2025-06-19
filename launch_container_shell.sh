b5b52e044d66e790b0d3c0daba9076f5f820b0e2
Jack S. Hale <mail@jackhale.co.uk>
2025-06-19 10:42:12 +0200

    Fix XDG error

--- launch_container_shell.sh
+++ launch_container_shell.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --rm -e PYVISTA_OFF_SCREEN=true -ti -v $(pwd):/shared -w /shared --entrypoint /bin/bash dolfinx/lab:nightly -l
+docker run --rm -e XDG_RUNTIME_DIR=/tmp -e PYVISTA_OFF_SCREEN=true -ti -v $(pwd):/shared -w /shared --entrypoint /bin/bash dolfinx/lab:nightly -l
