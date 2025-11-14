96481a16319e4753fb36efadf3d08bf64f4603b3
Jack S. Hale <mail@jackhale.co.uk>
2025-11-14 17:46:41 +0100

    First updates.

--- launch_container_shell.sh
+++ launch_container_shell.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --rm -e XDG_RUNTIME_DIR=/tmp -e PYVISTA_OFF_SCREEN=true -ti -v $(pwd):/shared -w /shared --entrypoint /bin/bash dolfinx/lab:nightly -l
+podman run --rm -e XDG_RUNTIME_DIR=/tmp -e PYVISTA_OFF_SCREEN=true -ti -v $(pwd):/shared -w /shared --entrypoint /bin/bash dolfinx/lab:v0.10.0-r1 -l
